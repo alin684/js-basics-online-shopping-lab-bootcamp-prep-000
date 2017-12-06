8bbf5bd779506d02c2d008ae752e9e5fbc069976
Abraham Lin <al684@cornell.edu>
2017-12-06 19:17:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (cart.length === 0) {
   }
   console.log(`In your cart, you have ${itemAndPriceList2.join(' and ')}.`);
 } else {
-  let lastItemAndPrice = cart[cart.length];
+  let lastItemAndPrice = cart[cart.length-1];
   let lastItem = Object.keys(lastItemAndPrice)[0];
   let lastPrice = lastItemAndPrice[lastItem];
   var itemAndPriceList3 = [];
