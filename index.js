8417d773b4fa2137a803d43cb8adadadaf4cace9
Abraham Lin <al684@cornell.edu>
2017-12-06 20:00:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,15 +63,16 @@ function total() {
 
 function removeFromCart(item) {
   var isThere = false;
-  for (let i = 0, i < cart.length; i++) {
+  for (let i = 0; i < cart.length; i++) {
     if (cart[i].hasOwnProperty(item)) {
-      var isThere = true;
-      cart.splice()
-  } else {
-    delete cart[item];
-    return cart;
+      isThere = true;
+      cart.splice(i, 1);
   }
+  }
+  if (isThere === false) {
   console.log("That item is not in your cart.");
+}
+return cart;
 }
 
 function placeOrder(cardNumber) {
