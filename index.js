2de0fcb9c3fed6f39b13f759231cc90cb3be623e
Abraham Lin <al684@cornell.edu>
2017-12-06 20:02:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,5 +76,9 @@ return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here}
+  if (cardNumber === null) {
+    console.log{"Sorry, we don't have a credit card on file for you.");
+    e
+  }
+}
 }
